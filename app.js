3b5f8a85899d190d371b53d47fa9a5f2fd4e23bf
Roy Sanfilipo <roysanfilipo@gmail.com>
2020-07-13 17:02:18 -0400

    can toggle guestbook info with button

--- app.js
+++ app.js
@@ -1,4 +1,75 @@
 
+class Guestbook extends React.Component {
+
+    state = {
+        show: false,
+        entries: []
+    }
+
+    toggleShow = () => {
+        this.setState({
+            show: !this.state.show
+        })
+    }
+
+    componentDidMount = () => {
+        axios.get('/entries').then(
+            (response) => {
+                this.setState({
+                    entries: response.data
+                })
+            }
+        )
+    }
+
+    render = () => {
+        const {entry, entries, createEntry,
+                changeNewEntryTitle,
+                changeNewEntryDate,
+                changeNewEntryNote } = this.props
+
+        return (
+            <div className="guestbookbutton">
+            <button onClick={this.toggleShow}>Guestbook</button>
+            <div className="guestbook">
+                {this.state.show ? (
+                  <div>
+                    <ul>
+                        {
+                            this.state.entries.map(
+                                (entry) => {
+                                    return (
+                                        <li>
+                                            <h4>{entry.title}</h4>
+                                            <h4>DATE: {entry.date}</h4>
+                                            {entry.note}<br/>
+                                        </li>
+                                    )
+                                }
+                            )
+                        }
+                    </ul>
+                    <div className="newentry">
+                      <h2>Sign the guestbook!</h2>
+                      <form className="newform" onSubmit={createEntry}>
+                          <input onChange={changeNewEntryTitle} type="text" placeholder="title"/>
+                          <input onChange={changeNewEntryDate} type="date" placeholder="date"/>
+                          <input onChange={changeNewEntryNote} type="textarea" placeholder="note"/>
+                          <input type="submit" value="Add"/>
+                      </form>
+                    </div>
+                  </div>
+                ) : (
+                    ''
+                )}
+
+            </div>
+            </div>
+        )
+    }
+}
+
+
 class App extends React.Component {
         state = {
             entries: []
@@ -44,15 +115,7 @@ class App extends React.Component {
 
     }
 
-    componentDidMount = () => {
-        axios.get('/entries').then(
-            (response) => {
-                this.setState({
-                    entries: response.data
-                })
-            }
-        )
-    }
+
 
 
     changeUpdateEntryTitle = (event) => {
@@ -104,55 +167,34 @@ class App extends React.Component {
 
     render = () => {
         return (
-    <div className="container">
-            <div className="header">
-                <h1>332 Ozone Walk</h1>
-            </div>
-            <div className="pagecontent">
-            <div className="sidebar">
-              <button>About</button>
-              <button>Photos</button>
-              <button>Features</button>
-              <button>House Rules</button>
-              <button>Contact</button>
-              <button>Guestbook</button>
-            </div>
-            <div className="main">
-                <div className="mainphoto">
-                  <img src="https://i.imgur.com/ke1l3L4.jpg" />
+          <div className="container">
+              <div className="header">
+                  <h1>332 Ozone Walk</h1>
+              </div>
+              <div className="pagecontent">
+                <div className="sidebar">
+                  <button>About</button>
+                  <button>Photos</button>
+                  <button>Features</button>
+                  <button>House Rules</button>
+                  <button>Contact</button>
+                  <button>Guestbook</button>
                 </div>
-                <div className="guestbook">
-                  <ul>
-                      {
-                          this.state.entries.map(
-                              (entry) => {
-                                  return (
-                                      <li>
-                                          <h4>{entry.title}</h4>
-                                          <h4>DATE: {entry.date}</h4>
-                                          {entry.note}<br/>
-                                      </li>
-                                  )
-                              }
-                          )
-                      }
-                  </ul>
-                  <div className="newentry">
-                    <h2>Sign the guestbook!</h2>
-                    <form className="newform" onSubmit={this.createEntry}>
-                        <input onChange={this.changeNewEntryTitle} type="text" placeholder="title"/>
-                        <input onChange={this.changeNewEntryDate} type="date" placeholder="date"/>
-                        <input onChange={this.changeNewEntryNote} type="textarea" placeholder="note"/>
-                        <input type="submit" value="Add"/>
-                    </form>
-                  </div>
+                <div className="main">
+                    <div className="mainphoto">
+                      <img src="https://i.imgur.com/ke1l3L4.jpg" />
+                    </div>
+                    <Guestbook
+                      createEntry={this.createEntry}
+                      changeNewEntryTitle={this.changeNewEntryTitle}
+                      changeNewEntryDate={this.changeNewEntryDate}
+                      changeNewEntryNote={this.changeNewEntryNote}
+                      entry={this.entry}/>
                 </div>
               </div>
             </div>
-        </div>
         )
     }
-
 }
 
 ReactDOM.render(
