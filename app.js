9506936427ae78a71f749ec733563449cdfbf904
Roy Sanfilipo <roysanfilipo@gmail.com>
2020-07-17 17:49:26 -0400

    site functions and all the buttons display in a nav bar beause of the new Nav class

--- app.js
+++ app.js
@@ -1,134 +1,86 @@
+class Nav extends React.Component {
+  render = () => {
+    const { toggleShowAbout, toggleShowPhotos,
+            toggleShowFeatures, toggleShowGuestbook,
+            toggleShowContact } = this.props
 
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <ul>
+            <li><button onClick={toggleShowAbout}>About</button></li>
+            <li><button onClick={toggleShowPhotos}>Photos</button></li>
+            <li><button onClick={toggleShowFeatures}>Features</button></li>
+            <li><button onClick={toggleShowGuestbook}>Guestbook</button></li>
+            <li><button onClick={toggleShowContact}>Contact</button></li>
+          </ul>
+        </div>
+      </nav>
+    )
+  }
+}
 
 class About extends React.Component {
-    state = {
-        show: false,
-    }
-
-    toggleShow = () => {
-        this.setState({
-            show: !this.state.show
-        })
-    }
-
     render = () => {
-
         return (
-          <div className="about">
-              <div className="aboutbutton">
-                <button onClick={this.toggleShow}>About</button>
-              </div>
-              <div>
-                  {this.state.show ? (
-                    <div>
-                      <p>This is random test content about 332 Ozone Walk in Fire Island Pines!<br/>
-                      Who really knows what to write here, you know? </p>
-                    </div>
-                  ) : (
-                      ''
-                  )}
-              </div>
+          <div className="aboutcontent">
+            <p>This is random test content about 332 Ozone Walk in Fire Island Pines!<br/>
+            Who really knows what to write here, you know? </p>
           </div>
         )
     }
 }
 
 class Photos extends React.Component {
-    state = {
-        show: false
-    }
-
-    toggleShow = () => {
-        this.setState({
-            show: !this.state.show
-        })
-    }
-
     render = () => {
-
         return (
-          <div className="photosection">
-              <div className="photosbutton">
-                <button onClick={this.toggleShow}>Photos</button>
-              </div>
-              <div>
-                  {this.state.show ? (
-                    <div className="allphotos">
-                      <div className="photoone">
-                        <img src="https://i.imgur.com/ke1l3L4.jpg" />
-                      </div>
-                      <div className="phototwo">
-                        <img src="https://i.imgur.com/ke1l3L4.jpg" />
-                      </div>
-                      <div className="photothree">
-                        <img src="https://i.imgur.com/ke1l3L4.jpg" />
-                      </div>
-                      <div className="photofour">
-                        <img src="https://i.imgur.com/ke1l3L4.jpg" />
-                      </div>
-                      <div className="photofive">
-                        <img src="https://i.imgur.com/ke1l3L4.jpg" />
-                      </div>
-                      <div className="photosix">
-                        <img src="https://i.imgur.com/ke1l3L4.jpg" />
-                      </div>
-                    </div>
-                  ) : (
-                      ''
-                  )}
-              </div>
+          <div className="allphotos">
+            <div className="photoone">
+              <img src="https://i.imgur.com/ke1l3L4.jpg" />
+            </div>
+            <div className="phototwo">
+              <img src="https://i.imgur.com/ke1l3L4.jpg" />
+            </div>
+            <div className="photothree">
+              <img src="https://i.imgur.com/ke1l3L4.jpg" />
+            </div>
+            <div className="photofour">
+              <img src="https://i.imgur.com/ke1l3L4.jpg" />
+            </div>
+            <div className="photofive">
+              <img src="https://i.imgur.com/ke1l3L4.jpg" />
+            </div>
+            <div className="photosix">
+              <img src="https://i.imgur.com/ke1l3L4.jpg" />
+            </div>
           </div>
         )
     }
 }
 
 class Features extends React.Component {
-    state = {
-        show: false,
-    }
-
-    toggleShow = () => {
-        this.setState({
-            show: !this.state.show
-        })
-    }
-
     render = () => {
-
         return (
           <div className="features">
-              <div className="featuresbutton">
-                <button onClick={this.toggleShow}>Features</button>
-              </div>
-              <div>
-                  {this.state.show ? (
-                    <ul>
-                      <li>Four bedrooms and three bathrooms</li>
-                      <li>Spacious kitchen with Allclad cookware and dishwasher</li>
-                      <li>Large pool, hot tub, and outdoor shower</li>
-                      <li>Washer and dryer</li>
-                      <li>Smart TV and indoor/outdoor Sonos soundsystem</li>
-                    </ul>
-                  ) : (
-                      ''
-                  )}
-              </div>
+            <ul>
+              <li>Four bedrooms and three bathrooms</li>
+              <li>Spacious kitchen with Allclad cookware and dishwasher</li>
+              <li>Large pool, hot tub, and outdoor shower</li>
+              <li>Washer and dryer</li>
+              <li>Smart TV and indoor/outdoor Sonos soundsystem</li>
+            </ul>
           </div>
         )
     }
 }
 
 class Contact extends React.Component {
-    state = {
-        show: false
-    }
-
-    toggleShow = () => {
-        this.setState({
-            show: !this.state.show
-        })
-    }
-
+  state = {
+    name: null,
+    email: null,
+    phone: null,
+    message: null,
+  }
     sendEmail = (event) => {
       event.preventDefault();
       emailjs.sendForm('contact_service', 'contact_form', event.target, 'user_C9Y31mNs8fuYKIEUYCxUP')
@@ -147,28 +99,16 @@ class Contact extends React.Component {
   }
 
     render = () => {
-
         return (
-          <div className="contact">
-              <div className="contactbutton">
-                <button onClick={this.toggleShow}>Contact</button>
-              </div>
-              <div>
-                  {this.state.show ? (
-                    <div>
-                      <form className="contact-form" onSubmit={this.sendEmail}>
-                          <input type="hidden" name="contact_number"/>
-                          <input type="text" name="user_name" value={this.state.name} placeholder="name" />
-                          <input type="email" name="user_email" value={this.state.email} placeholder="email"/>
-                          <input type="text" name="user_phone" value={this.state.phone} placeholder="phone #"/>
-                          <textarea name="message" value={this.state.message} placeholder="message"/>
-                          <input type="submit" value="Send" />
-                        </form>
-                    </div>
-                  ) : (
-                      ''
-                  )}
-              </div>
+          <div>
+            <form className="contact-form" onSubmit={this.sendEmail}>
+                <input type="hidden" name="contact_number"/>
+                <input type="text" name="user_name" value={this.state.name} placeholder="name" />
+                <input type="email" name="user_email" value={this.state.email} placeholder="email"/>
+                <input type="text" name="user_phone" value={this.state.phone} placeholder="phone #"/>
+                <textarea name="message" value={this.state.message} placeholder="message"/>
+                <input type="submit" value="Send" />
+            </form>
           </div>
         )
     }
@@ -176,15 +116,8 @@ class Contact extends React.Component {
 
 class Guestbook extends React.Component {
     state = {
-        show: false,
         admin: true,
         entries: []
-    }
-
-    toggleShow = () => {
-        this.setState({
-            show: !this.state.show
-        })
     }
 
     componentDidMount = () => {
@@ -198,58 +131,105 @@ class Guestbook extends React.Component {
     }
 
     render = () => {
-        const { refreshPage, entry, createEntry,
+        const { entry, createEntry,
                 changeNewEntryTitle,
                 changeNewEntryDate,
                 changeNewEntryNote,
                 deleteEntry } = this.props
 
         return (
-            <div className="guestbookbutton">
-            <button onClick={this.toggleShow}>Guestbook</button>
-            <div className="guestbook">
-                {this.state.show ? (
-                  <div>
-                    <ul>
-                        {
-                            this.state.entries.map(
-                                (entry) => {
-                                    return (
-                                        <li>
-                                            <h4>{entry.title}</h4>
-                                            <h4>DATE: {entry.date}</h4>
-                                            {entry.note}<br/>
-                                            {this.state.admin ? (
-                                              <button value={entry.id} onClick={deleteEntry} >DELETE</button>
-                                            ) : ( '' )}
-                                        </li>
-                                    )
-                                }
+          <div>
+            <ul>
+                {
+                    this.state.entries.map(
+                        (entry) => {
+                            return (
+                                <li>
+                                    <h4>{entry.title}</h4>
+                                    <h4>DATE: {entry.date}</h4>
+                                    {entry.note}<br/>
+                                    {this.state.admin ? (
+                                      <button value={entry.id} onClick={deleteEntry} >DELETE</button>
+                                    ) : ( '' )}
+                                </li>
                             )
                         }
-                    </ul>
-                    <div className="newentry">
-                      <h2>Sign the guestbook!</h2>
-                      <form className="newform" onSubmit={createEntry}>
-                          <input onChange={changeNewEntryTitle} type="text" placeholder="title"/>
-                          <input onChange={changeNewEntryDate} type="date" placeholder="date"/>
-                          <input onChange={changeNewEntryNote} type="textarea" placeholder="note"/>
-                          <input type="submit" value="Add"/>
-                      </form>
-                    </div>
-                  </div>
-                ) : (
-                    ''
-                )}
-
+                    )
+                }
+            </ul>
+            <div className="newentry">
+              <h2>Sign the guestbook!</h2>
+              <form className="newform" onSubmit={createEntry}>
+                  <input onChange={changeNewEntryTitle} type="text" placeholder="title"/>
+                  <input onChange={changeNewEntryDate} type="date" placeholder="date"/>
+                  <input onChange={changeNewEntryNote} type="textarea" placeholder="note"/>
+                  <input type="submit" value="Add"/>
+              </form>
             </div>
-            </div>
+          </div>
         )
     }
 }
 
 
 class App extends React.Component {
+  state = {
+      showabout: false,
+      showphotos: false,
+      showfeatures: false,
+      showguestbook: false,
+      showcontact: false
+  }
+
+  toggleShowAbout = () => {
+      this.setState({
+          showabout: !this.state.showabout,
+          showphotos: false,
+          showfeatures: false,
+          showguestbook: false,
+          showcontact: false
+      })
+  }
+
+  toggleShowPhotos = () => {
+      this.setState({
+          showphotos: !this.state.showphotos,
+          showabout: false,
+          showfeatures: false,
+          showguestbook: false,
+          showcontact: false
+      })
+  }
+
+  toggleShowFeatures = () => {
+      this.setState({
+          showfeatures: !this.state.showfeatures,
+          showabout: false,
+          showphotos: false,
+          showguestbook: false,
+          showcontact: false
+      })
+  }
+
+  toggleShowGuestbook = () => {
+      this.setState({
+          showguestbook: !this.state.showguestbook,
+          showphotos: false,
+          showfeatures: false,
+          showabout: false,
+          showcontact: false
+      })
+  }
+
+  toggleShowContact = () => {
+      this.setState({
+          showcontact: !this.state.showcontact,
+          showphotos: false,
+          showfeatures: false,
+          showguestbook: false,
+          showabout: false
+      })
+  }
 
     changeNewEntryTitle = (event) => {
         this.setState({
@@ -287,7 +267,6 @@ class App extends React.Component {
                 })
             }
         )
-
     }
 
     changeUpdateEntryTitle = (event) => {
@@ -343,33 +322,79 @@ class App extends React.Component {
           <div className="container">
               <div className="row">
                   <div className="col s12"><span className="flow-text">332 Ozone Walk</span></div>
+                  <Nav
+                    toggleShowAbout={this.toggleShowAbout}
+                    toggleShowPhotos={this.toggleShowPhotos}
+                    toggleShowFeatures={this.toggleShowFeatures}
+                    toggleShowGuestbook={this.toggleShowGuestbook}
+                    toggleShowContact={this.toggleShowContact}
+                  />
               </div>
-              <div className="divider"></div>
               <div className="pagecontent">
                 <div className="main">
                     <div className="mainphoto">
                       <img src="https://i.imgur.com/ke1l3L4.jpg" />
                     </div>
-                    <About/>
-                    <Photos/>
-                    <Features/>
-                    <Guestbook
-                      createEntry={this.createEntry}
-                      changeNewEntryTitle={this.changeNewEntryTitle}
-                      changeNewEntryDate={this.changeNewEntryDate}
-                      changeNewEntryNote={this.changeNewEntryNote}
-                      deleteEntry={this.deleteEntry}
-                      entry={this.entry}
-                      refreshPage={this.refreshPage}/>
-                    <Contact/>
+
+                    <div className="about">
+                        {this.state.showabout ? (
+                          <div>
+                            <About/>
+                          </div>
+                        ) : (
+                            ''
+                        )}
+                    </div>
+
+                    <div className="photos">
+                        <div>
+                            {this.state.showphotos ? (
+                              <Photos/>
+                            ) : (
+                                ''
+                            )}
+                        </div>
+                    </div>
+
+                    <div className="features">
+                        {this.state.showfeatures ? (
+                            <Features/>
+                        ) : (
+                            ''
+                        )}
+                    </div>
+
+                    <div className="guestbook">
+                        {this.state.showguestbook ? (
+                          <Guestbook
+                            createEntry={this.createEntry}
+                            changeNewEntryTitle={this.changeNewEntryTitle}
+                            changeNewEntryDate={this.changeNewEntryDate}
+                            changeNewEntryNote={this.changeNewEntryNote}
+                            deleteEntry={this.deleteEntry}
+                            entry={this.entry} />
+                        ) : (
+                            ''
+                        )}
+                    </div>
+
+                    <div className="contact">
+                            {this.state.showcontact ? (
+                              <Contact/>
+                            ) : (
+                                ''
+                            )}
+                    </div>
 
                 </div>
+
                 <footer>
                   <div className="socialmedia">
                     <a href="#"><img src="instagramlogoimage" /></a>
                   </div>
                   <div>© 2020 by <a href="https://github.com/roysanfilipo">Roy Sanfilipo</a></div>
                 </footer>
+
               </div>
             </div>
         )
